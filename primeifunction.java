9efe9204041fae5e5212ed3fb147503728dc2685
Shaik Aman Sayer <118007035+shaikamansayer13@users.noreply.github.com>
2023-02-10 01:10:54 +0530

    primefuction.java

---
+++ primeifunction.java
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.util.*;
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner sc = new Scanner (System.in);
+    int a = sc.nextInt ();
+    int flag = 0;
+    if (a == 0)
+      {
+	System.out.println ("Not Valid");
+      }
+    if (a == 1)
+      {
+	System.out.println ("Not Valid");
+      }
+    if (a > 2)
+      {
+	for (int i = 1; i <= a; i++)
+	  {
+	    if (a % i == 0)
+	      {
+		flag++;
+	      }
+	  }
+	if (flag > 2)
+	  {
+	    System.out.println ("not a  prime");
+	  }
+	else
+	  {
+	    System.out.println (" prime");
+	  }
+      }
+
+
+  }
+}
