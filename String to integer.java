15865e659e7ed25593606aa3504fed7cd3ec2c4c
Shaik Aman Sayer <118007035+shaikamansayer13@users.noreply.github.com>
2023-02-11 00:18:32 +0530

    Create String to integer.java

---
+++ String to integer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+  public static void main (String args[])
+  {
+    String str = "123";
+    int number = Integer.parseInt (str);
+      System.out.println (number);
+
+
+  }
+}
