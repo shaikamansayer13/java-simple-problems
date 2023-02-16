0f31e7296262faa1b940f6e6f7d2ac2a62cfd2ad
Shaik Aman Sayer <118007035+shaikamansayer13@users.noreply.github.com>
2023-02-17 02:00:01 +0530

    Create 1standlastOccurance.java

---
+++ 1standlastOccurance.java
@@ -0,0 +1,24 @@
+public class Main{
+public static int first = -1;
+public static int last = -1;
+public static void getIndices(String str, char el, int idx) {
+if(idx == str.length()) {
+return;
+}
+if(str.charAt(idx) == el) {
+if(first == -1) {
+first = idx;
+} else {
+last = idx;
+}
+}
+getIndices(str, el, idx+1);
+}
+public static void main(String args[]) {
+String str = "tabcdfgchydfchijakkk";
+char el = 'a';
+getIndices(str, el, 0);
+System.out.println("First occurence : " + first);
+System.out.println("Last occurence : " + last);
+}
+}
